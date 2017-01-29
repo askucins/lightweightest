2a0238a6a53b181e72634493dffe86055c378732
Adam Skucinski <askucins@gmail.com>
2017-01-29 11:19:13 +0100

    moved examples out; added dependency to a local maven cache

--- examples/Example.groovy
+++ examples/Example.groovy
@@ -1,19 +1,19 @@
 @GrabResolver(name = "lightweightest", m2Compatible = 'true', root = 'https://raw.github.com/askucins/lightweightest/develop/repository')
-@Grab("org.lightweightest:lightweightest:0.5")
+@Grab("org.lightweightest:lightweightest:0.5.1")
 import org.lightweightest.Lightweightest
 
-def server = Lightweightest.start(port: 9999) {
+def server = Lightweightest.start(port: 9999, stopAfter: 1) {
     get("/card/list") { request, response ->
         "qwerty"
     }
 }
 
 server.get("/test") { request, response ->
-    "qwerty ${request.params.id}"
+    "asdfgh ${request.params.id}"
 }
 
 println "http://localhost:9999/card/list".toURL().text
-println "http://localhost:9999/test?id=1001".toURL().text
+//println "http://localhost:9999/test?id=1001".toURL().text
 
 
 server.stop()
